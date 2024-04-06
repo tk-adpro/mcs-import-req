87db7fcda8d9309b217db0c42c6b899f068d441c
bryanjeshua <bryanjmt04@gmail.com>
2024-04-06 15:48:55 +0700

    initialization

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcsimportreq/McsImportReqApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.mcsimportreq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsImportReqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsImportReqApplication.class, args);
+    }
+
+}
