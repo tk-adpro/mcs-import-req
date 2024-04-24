dc2b0b517c2297f753444d1b8441a722e9a4e837
khalishana <khalisha.hana10@gmail.com>
2024-04-24 19:33:57 +0700

    [RED] Implement condition in request model to pass some tests

--- src/main/java/id/ac/ui/cs/advprog/eshop/mcsimportreq/model/Request.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcsimportreq/model/Request.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.mcsimportreq.model;
 
 import lombok.Data;
+import org.springframework.util.StringUtils;
 
 @Data
 public class Request {
@@ -20,10 +21,16 @@ public class Request {
     }
 
     public void setProductName(String productName) {
+        if (StringUtils.isEmpty(productName)) {
+            throw new IllegalArgumentException("Product name cannot be empty");
+        }
         this.productName = productName;
     }
 
     public void setImageUrl(String imageUrl) {
+        if (StringUtils.isEmpty(imageUrl)) {
+            throw new IllegalArgumentException("Image URL cannot be empty");
+        }
         this.imageUrl = imageUrl;
     }
 
@@ -35,10 +42,16 @@ public class Request {
     }
 
     public void setStoreUrl(String storeUrl) {
+        if (StringUtils.isEmpty(storeUrl)) {
+            throw new IllegalArgumentException("Store URL cannot be empty");
+        }
         this.storeUrl = storeUrl;
     }
 
     public void setCurrency(String currency) {
+        if (StringUtils.isEmpty(currency)) {
+            throw new IllegalArgumentException("Currency cannot be empty");
+        }
         this.currency = currency;
     }
 }
