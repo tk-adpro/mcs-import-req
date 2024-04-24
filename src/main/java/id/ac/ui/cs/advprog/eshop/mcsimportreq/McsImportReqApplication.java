46b67a300f5aab8383e283f6ca2f7d9ba74ccf65
khalishana <khalisha.hana10@gmail.com>
2024-04-24 17:12:59 +0700

    fix: Try deploy

--- src/main/java/id/ac/ui/cs/advprog/eshop/mcsimportreq/McsImportReqApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcsimportreq/McsImportReqApplication.java
@@ -2,8 +2,9 @@ package id.ac.ui.cs.advprog.eshop.mcsimportreq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class McsImportReqApplication {
 
     public static void main(String[] args) {
