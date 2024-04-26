da0cffbebfc1d1ebb2a4095e1ef74ebe7dedc894
khalishana <khalisha.hana10@gmail.com>
2024-04-26 07:53:05 +0700

    [GREEN] Implement request repository

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcsimportreq/repository/RequestRepository.java
@@ -0,0 +1,32 @@
+package id.ac.ui.cs.advprog.eshop.mcsimportreq.repository;
+
+import id.ac.ui.cs.advprog.eshop.mcsimportreq.model.Request;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RequestRepository {
+    private List<Request> requests = new ArrayList<>();
+
+    public Request save(Request request) {
+        for (int i = 0; i < requests.size(); i++) {
+            if (requests.get(i).getId().equals(request.getId())) {
+                requests.set(i, request);
+                return request;
+            }
+        }
+        requests.add(request);
+        return request;
+    }
+
+    public Request findRequestById(Long requestId) {
+        for (Request savedRequest : requests) {
+            if (savedRequest.getId().equals(savedRequest.getId())) {
+                return savedRequest;
+            }
+        }
+        return null;
+    }
+}
