e2f721b5c0af4e3bc81c286fd32020d378055500
khalishana <khalisha.hana10@gmail.com>
2024-05-15 18:02:51 +0700

    reset import req application

--- src/main/java/id/ac/ui/cs/advprog/eshop/mcsimportreq/McsImportReqApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcsimportreq/McsImportReqApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class McsImportReqApplication {
 
     public static void main(String[] args) {
