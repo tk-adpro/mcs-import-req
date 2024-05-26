e36235190d1453214a65261d66cec1ed7783b625
khalishana <khalisha.hana10@gmail.com>
2024-05-26 15:30:38 +0700

    add api in security configurer

--- src/main/java/id/ac/ui/cs/advprog/eshop/mcsimportreq/config/SecurityConfigurer.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcsimportreq/config/SecurityConfigurer.java
@@ -3,26 +3,32 @@ package id.ac.ui.cs.advprog.eshop.mcsimportreq.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-// Do not change this code
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfigurer {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()
-                );
+                .csrf(csrf -> csrf.disable()) // Disable CSRF protection for testing
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/requests/create").permitAll()
+                        .requestMatchers("/api/requests/edit/**").permitAll()
+                        .requestMatchers("/api/requests/delete/**").permitAll()
+                        .requestMatchers("/api/requests/{requestId}").permitAll()
+                        .requestMatchers("/api/requests").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Use stateless session management
+
         return http.build();
     }
 
