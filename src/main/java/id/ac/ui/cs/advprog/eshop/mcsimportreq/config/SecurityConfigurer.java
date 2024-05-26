d26ac8c71f3f11a3b455da6fbf8acb0a61923b01
khalishana <khalisha.hana10@gmail.com>
2024-05-26 12:02:57 +0700

    edit security configurer

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcsimportreq/config/SecurityConfigurer.java
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.advprog.eshop.mcsimportreq.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+// Do not change this code
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfigurer {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+}
